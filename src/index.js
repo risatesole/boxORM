b83f5ecc84edb7b6df5b3297bd428c2dd57540ec
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2025-05-16 10:37:17 -0400

    add implementation of insert logic just the returns, (just the basis) for multiple database dialects to insert data

--- src/index.js
+++ src/index.js
@@ -19,6 +19,31 @@ class box {
         return this;
     }
 
+    insert(insert_data) {
+        this.atribute;
+        const resoult = this.validateDataAgainstSchema(
+            insert_data,
+            this.atribute
+        );
+        if (resoult.isValid == false) {
+            return 'invalid data';
+        } else {
+            if (this.dialect == 'sqlite3') {
+                // todo: put insert sqlite3 logic here
+                return 'valid data - in sqlite3';
+            } else if (this.dialect == 'mysql') {
+                // todo: put mysql insert logic here
+                return 'valid data - in mysql'
+            } else if (this.dialect == 'mariadb') {
+                // todo: put mariadb insert logic here
+                return 'valid data - in mariadb'
+            }else {
+                return 'not dbms recognised';
+            }
+            return 'just valid data no dbms recognised';
+        }
+    }
+
     getIntAtributes() {
         for (const [key, value] of Object.entries(this.atribute)) {
             if (value.type === 'INTEGER') {
