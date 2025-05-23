169a53700c574f0ed2de33f8f36bbd84b6c7eed8
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2025-05-22 22:53:58 -0400

    refactor: example of using the orm updated

--- example/src/index.js
+++ example/src/index.js
@@ -1,160 +1,31 @@
-const box = require('box');
-const sqlite3 = require('sqlite3').verbose();
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-const Box = new box({
-    storage: './db.sqlite3',
-    dialect: 'template',
-    port: 88
-    
-},"myUsername","strongPassword");
+  getAlcohol() {
+    if (this.age >= 21) {
+      return `${this.name} (age ${this.age}) can buy alcohol.`;
+    } else {
+      return `${this.name} (age ${this.age}) is too young to buy alcohol.`;
+    }
+  }
+}
 
-const User = Box.define(
-    "user",
-    {
-        name: {
-            type: 'STRING',
-            allowNull: true,
-        },
-        age: {
-            type: 'INTEGER',
-            allowNull: false,
-        },
-        eyeColor: {
-            type: 'STRING',
-            allowNull: true,
-        },
-        IsDominican: {
-            type: 'BOOLEAN',
-            allowNull: false
-        }
-    },
-    'anymodeloption'
-);
+// Create a factory function
+function createPerson() {
+  return function(name, age) {
+    return new Person(name, age);
+  };
+}
 
+// Create the constructor only once
+const personFactory = createPerson();
 
+// Create multiple people
+const Alice = personFactory('alice', 24);
+const Bob = personFactory('bob', 18);
 
-
-
-const Shoe = Box.define(
-    "shoe",
-    {
-        size: {
-            type: 'INTEGER',
-            allowNull: true,
-        },
-        modelname: {
-            type: 'STRING',
-            allowNull: false,
-        },
-        color: {
-            type: 'STRING',
-            allowNull: true,
-        }
-    },
-    'anymodeloption'
-);
-
-// User.insert({
-//     name: "allis",
-//     age: 25,
-//     eyeColor: "red",
-//     IsDominican: true
-// });
-
-
-
-
-// try{
-// await User.insert({
-//     name: "John",
-//     age: 25,
-//     eyeColor: "brown",
-//     IsDominican: true
-// });
-// }catch{
-//     console.error(err);
-    
-// }
-
-
-
-
-
-
-// // function myquery(tablename, ColumnDesign) {
-
-// //     const mytable = tablename;
-    
-// //     // Process each column definition
-// //     const columns = Object.entries(ColumnDesign).map(([columnName, columnDef]) => {
-// //         let columnQuery = `${columnName} `;
-        
-// //         // Map JavaScript types to SQLite types
-// //         switch(columnDef.type.toUpperCase()) {
-// //             case 'STRING':
-// //                 columnQuery += 'TEXT';
-// //                 break;
-// //             case 'INTEGER':
-// //                 columnQuery += 'INTEGER';
-// //                 break;
-// //             case 'BOOLEAN':
-// //                 columnQuery += 'INTEGER'; // SQLite doesn't have BOOLEAN, use INTEGER (0 or 1)
-// //                 break;
-// //             default:
-// //                 columnQuery += 'TEXT'; // Default to TEXT if type not recognized
-// //         }
-        
-// //         // Add constraints
-// //         if (columnName.toLowerCase() === 'id') {
-// //             columnQuery += ' PRIMARY KEY AUTOINCREMENT';
-// //         }
-        
-// //         if (columnDef.allowNull === false) {
-// //             columnQuery += ' NOT NULL';
-// //         }
-        
-// //         return columnQuery;
-// //     });
-    
-// //     // Join all column definitions with commas
-// //     const myColumnDesign = columns.join(', ');
-    
-// //     // Construct the final query
-// //     const query = `CREATE TABLE IF NOT EXISTS ${mytable} (${myColumnDesign});`;
-// //     console.log(query);
-// //     return query;
-// // }
-
-// // user = {
-// //     name: {
-// //         type: 'STRING',
-// //         allowNull: true,
-// //     },
-// //     age: {
-// //         type: 'INTEGER',
-// //         allowNull: false,
-// //     },
-// //     eyeColor: {
-// //         type: 'STRING',
-// //         allowNull: true,
-// //     },
-// //     IsDominican: {
-// //         type: 'BOOLEAN',
-// //         allowNull: false,
-// //     },
-// // };
-
-// // myquery('users', user);
-
-
-
-
-
-
-
-// listOfSuppourtedDialects=["english","french","italian","spanish"];
-// dialectUsing="spanish";
-
-// if (!listOfSuppourtedDialects.includes(dialectUsing)) {
-//     console.log(` is not in the array`);
-// }else {}
+console.log(Alice.getAlcohol()); // "alice (age 24) can buy alcohol."
+console.log(Bob.getAlcohol());   // "bob (age 18) is too young to buy alcohol."
